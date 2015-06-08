e286749d92fde970ec1fafd9a68e594b296a38ac
Jeffry-Elite <jeffry@elitemobile.co.id>
2015-06-08 13:00:21 +0700

    Fix Layout for video tab - Add title for video - Change color - Add Arial bold for font type - Resize image in adapter using glide

--- app/src/main/java/com/mobile/elite/meowmeow/MainActivity.java
+++ app/src/main/java/com/mobile/elite/meowmeow/MainActivity.java
@@ -1,74 +1,110 @@
 package com.mobile.elite.meowmeow;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mobile.elite.meowmeow.adapter.image.TabsPagerAdapter;
 
 
-public class MainActivity extends ActionBarActivity implements ActionBar.TabListener{
+public class MainActivity extends ActionBarActivity implements ViewPager.OnPageChangeListener, View.OnClickListener {
 
     private ViewPager viewPager;
     private ActionBar actionBar;
     private TabsPagerAdapter tabAdapter;
-    private String[] tabtitle = {"Video", "Image"};
+    private String[] tabtitle ;
+    private TextView txtVideoTitle;
+    private TextView txtImageTitle;
+    private Typeface arialType;
+    private View pointerVideo;
+    private View pointerImage;
+    private int VIDEO_TAG = 0;
+    private int IMAGE_TAG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
         setContentView(R.layout.activity_main);
-        actionBar = getSupportActionBar();
         viewPager = (ViewPager)findViewById(R.id.pager);
         tabAdapter = new TabsPagerAdapter(getSupportFragmentManager());
-
+        txtVideoTitle = (TextView)findViewById(R.id.video_title_tab);
+        txtImageTitle = (TextView)findViewById(R.id.picture_title_tab);
         viewPager.setAdapter(tabAdapter);
+        viewPager.addOnPageChangeListener(this);
+        arialType = Typeface.createFromAsset(getAssets(),"fonts/arial_bold.ttf");
+        txtVideoTitle.setTypeface(arialType);
+        txtImageTitle.setTypeface(arialType);
+        pointerVideo = (View)findViewById(R.id.pointer_video);
+        pointerImage = (View)findViewById(R.id.pointer_picture);
 
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        txtVideoTitle.setOnClickListener(this);
+        txtImageTitle.setOnClickListener(this);
+        txtVideoTitle.setTag(VIDEO_TAG);
+        txtImageTitle.setTag(IMAGE_TAG);
 
-        for (String tab_name : tabtitle){
-            actionBar.addTab(actionBar.newTab().setText(tab_name)
-                    .setTabListener(this));
+        setPointerTab(0); // always set pointer to video tab at beginning
+    }
+
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        setPointerTab(position);
+    }
+
+
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    // 0 for video tab
+    // 1 for image tab
+    private void setPointerTab(int position) {
+        switch (position) {
+            case 0: {
+                pointerVideo.setVisibility(View.VISIBLE);
+                pointerImage.setVisibility(View.INVISIBLE);
+            }
+            break;
+            case 1: {
+                pointerVideo.setVisibility(View.INVISIBLE);
+                pointerImage.setVisibility(View.VISIBLE);
+            }
+            break;
         }
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        if(v.getTag() == VIDEO_TAG){
+            if(viewPager.getCurrentItem() == VIDEO_TAG)
+                return; // do nothing if current tab is in video
+           setTabAndPosition(0);
+        } else if(v.getTag() == IMAGE_TAG){
+            if(viewPager.getCurrentItem() == IMAGE_TAG)
+                return; // do nothing if current tab is in image
+            setTabAndPosition(1);
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
-            viewPager.setCurrentItem(tab.getPosition());
     }
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft) {
-
+    private void setTabAndPosition(int position){
+        setPointerTab(position);
+        viewPager.setCurrentItem(position);
     }
 }
--- app/src/main/java/com/mobile/elite/meowmeow/adapter/video/GridVideoAdapter.java
+++ app/src/main/java/com/mobile/elite/meowmeow/adapter/video/GridVideoAdapter.java
@@ -1,13 +1,20 @@
 package com.mobile.elite.meowmeow.adapter.video;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.mobile.elite.meowmeow.R;
 import com.mobile.elite.meowmeow.listener.ImageClickListener;
 
@@ -54,42 +61,55 @@ public class GridVideoAdapter extends BaseAdapter {
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
+        final ViewHolder viewHolder;
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.video_item_grid, null);
             viewHolder = new ViewHolder();
             viewHolder.imageHolder = (ImageView)convertView.findViewById(R.id.image_video);
+            viewHolder.textHolder = (TextView)convertView.findViewById(R.id.txt_title_video);
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder)convertView.getTag();
         }
 
         String url = "";
+        String title = "";
         try {
             url = jsData.getJSONObject(position).getString("160x120");
+            title = jsData.getJSONObject(position).getString("title");
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        viewHolder.textHolder.setText(title);
 
         viewHolder.imageHolder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    if(listener != null)
-                    listener.onImageClick(jsData.getJSONObject(position),jsData, position);
+                    if (listener != null)
+                        listener.onImageClick(jsData.getJSONObject(position), jsData, position);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
 
-        Glide.with(context).load(url).fitCenter().into(viewHolder.imageHolder);
+        Glide.with(context).load(url)
+                .asBitmap()
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                        viewHolder.imageHolder.setBackgroundDrawable(new BitmapDrawable(context.getResources(), resource));
+                    }
+                });
 
         return convertView;
     }
 
     private class ViewHolder{
         ImageView imageHolder;
+        TextView textHolder;
     }
 }
