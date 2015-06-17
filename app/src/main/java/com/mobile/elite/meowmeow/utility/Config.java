6e08b439a01d1710074d77b1cae6d9733c6edb23
Jeffry-Elite <jeffry@elitemobile.co.id>
2015-06-17 14:03:58 +0700

    Change API Link for video and image

--- app/src/main/java/com/mobile/elite/meowmeow/utility/Config.java
+++ app/src/main/java/com/mobile/elite/meowmeow/utility/Config.java
@@ -5,6 +5,6 @@ package com.mobile.elite.meowmeow.utility;
  */
 public class Config {
 
-    public static final String videoUrl = "http://k.clipbaru.com/api.php?c=get_listing&m=app_video&view=latest&api_from=android&agt_id=7018&page={page}&limit=20";
-    public static final String imageUrl = "http://c.grabinphone.com/api.php?c=get_list_photo&m=app_album&album_id=UUMLQ&api_from=android&agt_id=1080&view=random&page={page}&limit=20";
+    public static final String videoUrl = "http://k.clipbaru.com/api.php?c=get_listing&m=app_video&view=latest&api_from=android&agt_id=7023&rating=S&page={page}&limit=20";
+    public static final String imageUrl = "http://k.clipbaru.com/api.php?c=get_list_photo&m=app_album&album_id=UUMLQ&api_from=android&agt_id=7024&view=latest&page={page}&limit=20";
 }
